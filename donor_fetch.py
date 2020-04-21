0d5470a534d150da4288d77b70063be0a9c5d3ef
Mike Stark <mstark@factset.com>
2020-04-21 10:22:54 -0400

    Found the campaign id for M:OM and could finish script

--- donor_fetch.py
+++ donor_fetch.py
@@ -1,6 +1,17 @@
 import argparse
 import requests
 
+class Donation: # pylint: disable=R0903
+  """Represents a single donation to tiltify"""
+  def __init__(self, dataObject):
+    self.id = dataObject['id']
+    self.amount = dataObject['amount']
+    self.name = dataObject['name']
+    self.comment = dataObject['comment']
+
+  def __str__(self):
+    return f'{self.name} donated ${self.amount} and said "{self.comment}"'
+
 def get_access_token():
   """Reads the access token from .accesstoken"""
   token = None
@@ -11,25 +22,57 @@ def get_access_token():
 def make_tiltify_request(query_string):
   """Makes a request to tiltify"""
   url = make_tiltify_request.BASE_URL + query_string
-  headers = { 'Authorization': f'Bearer {get_access_token()}' }
-  return requests.get(url=url, headers=headers)
+  headers = { 'Authorization': f'Bearer {get_access_token()}' } # pylint: disable=C0326
+  return requests.get(url=url, headers=headers).json()
 
 make_tiltify_request.BASE_URL = 'https://tiltify.com/api/v3/'
 
 def get_campaign(campaign_id):
+  """Gets details of a campaign"""
   query_string = f'campaigns/{campaign_id}'
   request = make_tiltify_request(query_string)
   print(request)
 
-def get_donations(campaign_id):
-  query_string = f'campaigns/{campaign_id}/donations'
-  request = make_tiltify_request(query_string)
-  print(request)
+def get_donors(donations):
+  donor_names = dict()
+  for d in donations:
+    if d.name in donor_names:
+      donor_names[d.name] += d.amount
+    else:
+      donor_names[d.name] = d.amount
+
+  return donor_names
+
+def get_donations(campaign_id, count):
+  """Gets a list of donations"""
+  query_string = f'campaigns/{campaign_id}/donations?count={count}'
+  response = make_tiltify_request(query_string)
+  if response['meta']['status'] != 200:
+    return None
+  return [Donation(d) for d in response['data']]
 
 def main():
+  """Prints a list of donors"""
   parser = argparse.ArgumentParser()
+  parser.add_argument('-c', '--campaign-id', type=int)
+  parser.add_argument('-n', '--num-donations', type=int, default=100)
   args = parser.parse_args()
-  get_campaign('metroid_marathon')
+  donation_list = get_donations(args.campaign_id, args.num_donations)
+
+  if len(donation_list) > 0:
+    # Print Donor list
+    donors = get_donors(donation_list)
+    for name in sorted(donors):
+      print(f'{name} donated ${donors[name]}')
+
+    # Print Total
+    total = sum(map(lambda d: d.amount, donation_list))
+    print(f'Total: ${total}')
+
+    # Print Average
+    print(f'Average: ${total / len(donation_list)}')
+  else:
+    print('No donations yet!')
 
 if __name__ == "__main__":
   main()
